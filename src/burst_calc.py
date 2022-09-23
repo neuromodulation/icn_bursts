49f50e69419650d1f1b2565a310c0b6c3de85717
ali-dzaye <82215916+ali-dzaye@users.noreply.github.com>
2022-09-23 22:55:30 +0200

    update

--- src/burst_calc.py
+++ src/burst_calc.py
@@ -2,6 +2,7 @@ from os import stat
 import numpy as np
 from scipy import stats
 import mne
+import numpy
 
 LOW_BETA = (13, 21)
 HIGH_BETA = (20, 36)
@@ -44,7 +45,7 @@ def z_score(l_beta):
 
 
 def percentile(l_beta, percentile):
-    return [np.percentile(l_ch, q=percentile) for l_ch in l_beta]
+    return [np.percentile(l_beta, q=percentile)] # for l_ch in l_beta]
 
 
 def get_burst_length(beta_averp_norm, beta_thr, sfreq=250):
@@ -101,4 +102,62 @@ def get_burst_amplitude(beta_amplitude, beta_thr):
     mean_amplitude = np.mean(mean_amplitude_single_burst)
 
     return mean_amplitude
+
+def smooth(x,window_len=50,window='hamming'):
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+        
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+    
+    see also: 
+    
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    #if x.ndim != 1:
+    #    raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    #if x.size < window_len:
+     #   raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    #if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+     #   raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=numpy.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=numpy.ones(window_len,'d')
+    else:
+        w=eval('numpy.'+window+'(window_len)')
+
+    y=numpy.convolve(w/w.sum(),s,mode='valid')
+    return y
+
 
