f4089daa8580b44d4d31f43130796040c37c8acf
ali-dzaye <82215916+ali-dzaye@users.noreply.github.com>
2022-12-04 18:58:26 +0100

    up

--- berlin_pipeline.py
+++ berlin_pipeline.py
@@ -24,12 +24,12 @@ def main():
     m1_ids = project_constants["M1_IDS"]
     new_ch_names_map = project_constants["NEW_CH_NAMES_MAP"]
     files = project_constants["files"]
-    remove_subjects: Union[str, None] = ["001","002",'010'] 
+    remove_subjects: Union[str, None] = ["001", "002", "010"]
     if remove_subjects:
         for remove_subject in remove_subjects:
             files = [file for file in files if remove_subject not in file]
     files = preprocessing.pick_runs(files)
-    files_x = [f for f in files if "015" in f]
+    files_x = [f for f in files if "003" in f]
 
     # Define variables
     burst_char_pd_all = []
@@ -86,7 +86,7 @@ dist = pd.concat(M1_burst_dynamics_all)
 psd = pd.concat(npow_list_all)
 
 
-#plot_utils.plot_avgm1_burst_features(features)
+# plot_utils.plot_avgm1_burst_features(features)
 
 print("done")
 
@@ -94,66 +94,65 @@ print("done")
 avg_features = postprocessing.avg_features_sub(burst_char_pd_all)
 df_gavg_dist, df_sub_dist = postprocessing.avg_distribution(M1_burst_dynamics_all)
 
-#plot_utils.plot_avgm1_burst_features(avg_features)
-#plot_utils.plot_m1_burst_features(features)
+# plot_utils.plot_avgm1_burst_features(avg_features)
+# plot_utils.plot_m1_burst_features(features)
 
 # Distribution of Duration
-#plot_utils.plot_distribution(df_gavg_dist, df_sub_dist)
+# plot_utils.plot_distribution(df_gavg_dist, df_sub_dist)
 
 print("done")
 
 
 # Statistic
-on = features[features['Medication']=='On']
-off = features[features['Medication']=='Off']
-duration_on = on['Duration (s)']
-duration_off = off['Duration (s)']
+on = features[features["Medication"] == "On"]
+off = features[features["Medication"] == "Off"]
+duration_on = on["Duration (s)"]
+duration_off = off["Duration (s)"]
 res = wilcoxon(duration_off, duration_on)
 res.statistic, res.pvalue
 
 print("done")
 
-#def statistic(duration_on, duration_off), axis):
-#statistic = np.mean(duration_on) - np.mean(duration_off)
+# def statistic(duration_on, duration_off), axis):
+# statistic = np.mean(duration_on) - np.mean(duration_off)
 
 
-#from scipy.stats import permutation_test
+# from scipy.stats import permutation_test
 # because our statistic is vectorized, we pass `vectorized=True`
 # `n_resamples=np.inf` indicates that an exact test is to be performed
-#resi = permutation_test((duration_on, duration_off), statistic, vectorized=True,
-              #         n_resamples=np.inf, alternative='less')
-#print(res.statistic)
+# resi = permutation_test((duration_on, duration_off), statistic, vectorized=True,
+#         n_resamples=np.inf, alternative='less')
+# print(res.statistic)
 
-#print(resi.pvalue)
+# print(resi.pvalue)
 
 
-# Plot Distribution single subject 
-dist3 = df_sub_dist[df_sub_dist['Subject']==3]
-dist4 = df_sub_dist[df_sub_dist['Subject']==4]
-dist5 = df_sub_dist[df_sub_dist['Subject']==5]
-dist6 = df_sub_dist[df_sub_dist['Subject']==6]
-dist7 = df_sub_dist[df_sub_dist['Subject']==7]
-dist8 = df_sub_dist[df_sub_dist['Subject']==8]
-dist9 = df_sub_dist[df_sub_dist['Subject']==9]
-dist11 = df_sub_dist[df_sub_dist['Subject']==11]
-dist12 = df_sub_dist[df_sub_dist['Subject']==12]
-dist13 = df_sub_dist[df_sub_dist['Subject']==13]
-dist14 = df_sub_dist[df_sub_dist['Subject']==14]
-dist15 = df_sub_dist[df_sub_dist['Subject']==15]
+# Plot Distribution single subject
+dist3 = df_sub_dist[df_sub_dist["Subject"] == 3]
+dist4 = df_sub_dist[df_sub_dist["Subject"] == 4]
+dist5 = df_sub_dist[df_sub_dist["Subject"] == 5]
+dist6 = df_sub_dist[df_sub_dist["Subject"] == 6]
+dist7 = df_sub_dist[df_sub_dist["Subject"] == 7]
+dist8 = df_sub_dist[df_sub_dist["Subject"] == 8]
+dist9 = df_sub_dist[df_sub_dist["Subject"] == 9]
+dist11 = df_sub_dist[df_sub_dist["Subject"] == 11]
+dist12 = df_sub_dist[df_sub_dist["Subject"] == 12]
+dist13 = df_sub_dist[df_sub_dist["Subject"] == 13]
+dist14 = df_sub_dist[df_sub_dist["Subject"] == 14]
+dist15 = df_sub_dist[df_sub_dist["Subject"] == 15]
 
-#plot_utils.plot_distribution_sub3(dist3)
-#plot_utils.plot_distribution_sub4(dist4)
-#plot_utils.plot_distribution_sub5(dist5)
-#plot_utils.plot_distribution_sub6(dist6)
-#plot_utils.plot_distribution_sub7(dist7)
-#plot_utils.plot_distribution_sub8(dist8)
-#plot_utils.plot_distribution_sub9(dist9)
-#plot_utils.plot_distribution_sub11(dist11)
-#plot_utils.plot_distribution_sub12(dist12)
-#plot_utils.plot_distribution_sub13(dist13)
-#plot_utils.plot_distribution_sub14(dist14)
-#plot_utils.plot_distribution_sub15(dist15)
-
+# plot_utils.plot_distribution_sub3(dist3)
+# plot_utils.plot_distribution_sub4(dist4)
+# plot_utils.plot_distribution_sub5(dist5)
+# plot_utils.plot_distribution_sub6(dist6)
+# plot_utils.plot_distribution_sub7(dist7)
+# plot_utils.plot_distribution_sub8(dist8)
+# plot_utils.plot_distribution_sub9(dist9)
+# plot_utils.plot_distribution_sub11(dist11)
+# plot_utils.plot_distribution_sub12(dist12)
+# plot_utils.plot_distribution_sub13(dist13)
+# plot_utils.plot_distribution_sub14(dist14)
+# plot_utils.plot_distribution_sub15(dist15)
 
 (
     psd_s3off,
--- src/pipeline.py
+++ src/pipeline.py
@@ -37,50 +37,52 @@ def bursts_single_run(
             run=run,
         )
     )
-    if sub == '004':
+    if sub == "004":
         raw_annots = raw.set_annotations(annotations)
     else:
-        raw_annots = raw.set_annotations(preprocessing.check_annots_orig_time(annotations))
-    
-    if sub == '003':
+        raw_annots = raw.set_annotations(
+            preprocessing.check_annots_orig_time(annotations)
+        )
+
+    if sub == "003":
         raw_lfp = preprocessing.pick_lfp3(raw_annots)
-    if sub == '004':
+    if sub == "004":
         raw_lfp = preprocessing.pick_lfp4(raw_annots)
-    if sub == '008':
+    if sub == "008":
         raw_lfp = preprocessing.pick_lfp8(raw_annots)
-    if sub == '009':
+    if sub == "009":
         raw_lfp = preprocessing.pick_lfp9(raw_annots)
-    if sub == '015':
+    if sub == "015":
         raw_lfp = preprocessing.pick_lfp15(raw_annots)
-    if sub == '005':
+    if sub == "005":
         raw_lfp = preprocessing.pick_lfp_other(raw_annots)
-    if sub == '006':
+    if sub == "006":
         raw_lfp = preprocessing.pick_lfp_other(raw_annots)
-    if sub == '007':
+    if sub == "007":
         raw_lfp = preprocessing.pick_lfp_other(raw_annots)
-    if sub == '011':
+    if sub == "011":
         raw_lfp = preprocessing.pick_lfp_other(raw_annots)
-    if sub == '012':
+    if sub == "012":
         raw_lfp = preprocessing.pick_lfp12(raw_annots)
-    if sub == '013':
+    if sub == "013":
         raw_lfp = preprocessing.pick_lfp13(raw_annots)
-    if sub == '014':
+    if sub == "014":
         raw_lfp = preprocessing.pick_lfp_other(raw_annots)
-    #else:
+    # else:
     #    raw_lfp = preprocessing.pick_lfp_other(raw_annots)
 
-    raw_lfp_bi = preprocessing.bipolar_reference_lfp(raw_annots, raw_lfp, 'LFP')
+    raw_lfp_bi = preprocessing.bipolar_reference_lfp(raw_annots, raw_lfp, "LFP")
 
     NUM_CH = len(raw_lfp_bi.ch_names)
 
     raw_lfp_filt = preprocessing.filtering(raw_lfp_bi)
 
-    # plot recording and save annotation
-    #raw_lfp_filt.pick_channels(['LFP_R_1_8', "ECOG_L_1_2_SMC_AT"]).plot()
-    #print('done')
-    #raw_lfp_filt.annotations.save('sub-003_ses-EcogLfpMedOn03_task-Rest_acq-StimOff_run-1_annotations.csv', overwrite=True)
-
     raw_lfp_dow = preprocessing.downsample(raw_lfp_filt)
+
+    # plot recording and save annotation
+    raw_lfp_dow.pick_channels(["LFP", "ECOG_L_1_2_SMC_AT"]).plot()
+    print("done")
+    # raw_lfp_dow.annotations.save('sub-003_ses-EcogLfpMedOn03_task-Rest_acq-StimOff_run-1_annotations.csv', overwrite=True)
 
     signal = preprocessing.get_data(raw_lfp_dow)
 
@@ -89,39 +91,38 @@ def bursts_single_run(
     run_TF = burst_calc.Time_Frequency_Estimation(stand_signal)
 
     # list of beta bands
-    if sub == '003':
+    if sub == "003":
         l_beta = burst_calc.beta_bands_sub3(run_TF)
-    if sub == '004':
+    if sub == "004":
         l_beta = burst_calc.beta_bands_sub4(run_TF)
-    if sub == '005':
+    if sub == "005":
         l_beta = burst_calc.beta_bands_sub5(run_TF)
-    if sub == '006':
+    if sub == "006":
         l_beta = burst_calc.beta_bands_sub6(run_TF)
-    if sub == '007':
+    if sub == "007":
         l_beta = burst_calc.beta_bands_sub7(run_TF)
-    if sub == '008':
+    if sub == "008":
         l_beta = burst_calc.beta_bands_sub8(run_TF)
-    if sub == '009':
+    if sub == "009":
         l_beta = burst_calc.beta_bands_sub9(run_TF)
-    if sub == '010':
+    if sub == "010":
         l_beta = burst_calc.beta_bands_sub10(run_TF)
-    if sub == '011':
+    if sub == "011":
         l_beta = burst_calc.beta_bands_sub11(run_TF)
-    if sub == '012':
+    if sub == "012":
         l_beta = burst_calc.beta_bands_sub12(run_TF)
-    if sub == '013':
+    if sub == "013":
         l_beta = burst_calc.beta_bands_sub13(run_TF)
-    if sub == '014':
+    if sub == "014":
         l_beta = burst_calc.beta_bands_sub14(run_TF)
-    if sub == '015':
+    if sub == "015":
         l_beta = burst_calc.beta_bands_sub15(run_TF)
 
-    
     theta = 0
     mu = 1
     low = 2
     high = 3
-    #full = 4
+    # full = 4
 
     # Averaging power in all beta bands
     l_beta_avg = [burst_calc.avg_power(l) for l in l_beta]
@@ -129,7 +130,7 @@ def bursts_single_run(
     # Z-Scored averaged beta traces
     l_beta_avg_norm = [burst_calc.z_score(l) for l in l_beta_avg]
 
-    #CHOOSING BETA BAND
+    # CHOOSING BETA BAND
     # smoothing traces
     l_beta_smooth = [burst_calc.smooth(l) for l in l_beta_avg_norm[low]]
 
@@ -137,11 +138,11 @@ def bursts_single_run(
     l_beta_thr = [burst_calc.percentile(l, percentile=75) for l in l_beta_smooth]
 
     # Plot Signal
-    #signals_array, time_array = raw_lfp_dow[:, :]
-    #plt.plot (l_beta_smooth[-1], color='b')
-    #plt.axhline(l_beta_thr[-1], color='r', linestyle='--')
-    #sns.despine()
-    #print('done')
+    # signals_array, time_array = raw_lfp_dow[:, :]
+    # plt.plot (l_beta_smooth[-1], color='b')
+    # plt.axhline(l_beta_thr[-1], color='r', linestyle='--')
+    # sns.despine()
+    # print('done')
 
     # 2. CALCULATING FEATURES (NORMALIZED POWER, BURST LENGTH, BURST DYNAMIC) AND BIOMARKER COMPARISON #
     # Power spectral density
@@ -153,7 +154,7 @@ def bursts_single_run(
     power_spectra_norm = [
         p_ch / np.sum(p_ch[4:45] + p_ch[54:95]) for p_ch in power_spectra
     ]
-    psd_lfp = power_spectra_norm[len(raw_lfp_bi.ch_names)-1]
+    psd_lfp = power_spectra_norm[len(raw_lfp_bi.ch_names) - 1]
 
     # Burst duration
     burst_duration = [
@@ -164,25 +165,25 @@ def bursts_single_run(
         burst_calc.exclude_short_bursts(burst_duration[ch_idx])
         for ch_idx in range(NUM_CH)
     ]
-    burst_duration_cl_lfp = burst_duration_cl[len(raw_lfp_bi.ch_names)-1]
+    burst_duration_cl_lfp = burst_duration_cl[len(raw_lfp_bi.ch_names) - 1]
     # Mean burst duration
     mean_burst_duration = [
         np.nanmean(burst_duration_cl[ch_idx], axis=0) for ch_idx in range(NUM_CH)
     ]
-    mean_dur_lfp = mean_burst_duration[len(raw_lfp_bi.ch_names)-1]
-    
+    mean_dur_lfp = mean_burst_duration[len(raw_lfp_bi.ch_names) - 1]
+
     # Histogram of burst duration
     histogram_duration = [
         np.histogram(burst_duration_cl[ch_idx], density=False, bins=20, range=(0, 2))[0]
         for ch_idx in range(NUM_CH)
     ]
-    hist_dur_m1 = histogram_duration[len(raw_lfp_bi.ch_names)-1]
+    hist_dur_m1 = histogram_duration[len(raw_lfp_bi.ch_names) - 1]
     # Normed Histogram
     n_hist_dur = [
         100 * histogram_duration[ch_idx] / len(burst_duration_cl[ch_idx])
         for ch_idx in range(NUM_CH)
     ]
-    n_hist_dur_lfp = n_hist_dur[len(raw_lfp_bi.ch_names)-1]
+    n_hist_dur_lfp = n_hist_dur[len(raw_lfp_bi.ch_names) - 1]
     n_hist_dur_lfp_cl = np.delete(n_hist_dur_lfp, 0)
     short_bursts = n_hist_dur_lfp_cl[:7]
     prol_bursts = np.sum(n_hist_dur_lfp_cl[7:])
@@ -193,10 +194,12 @@ def bursts_single_run(
         burst_calc.get_burst_amplitude(l, l_beta_thr[idx])
         for idx, l in enumerate(l_beta_smooth)
     ]
-    burst_amplitude_lfp = burst_amplitude[len(raw_lfp_bi.ch_names)-1]
+    burst_amplitude_lfp = burst_amplitude[len(raw_lfp_bi.ch_names) - 1]
 
     # Burst Rate
-    burst_rate_lfp = np.sum(histogram_duration[len(raw_lfp_bi.ch_names)-1] / raw_annots.times[-1])
+    burst_rate_lfp = np.sum(
+        histogram_duration[len(raw_lfp_bi.ch_names) - 1] / raw_annots.times[-1]
+    )
 
     # 3. STRUCTURE FEATURES #
     # Burst Features
